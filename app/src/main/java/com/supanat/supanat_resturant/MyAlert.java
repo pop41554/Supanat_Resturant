a673fb188246cd54e967f3b2f13f26e4a130bc0c
pop41554 <pop41554@hotmail.com>
2016-11-30 09:26:45 +0700

    Initial commit

---
+++ app/src/main/java/com/supanat/supanat_resturant/MyAlert.java
@@ -0,0 +1,29 @@
+package com.supanat.supanat_resturant;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by Pc on 2/11/2559.
+ */
+public class MyAlert {
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setIcon(R.drawable.danger);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+}
---
+++ app/src/main/java/com/supanat/supanat_resturant/OrderTABLE.java
@@ -0,0 +1,34 @@
+package com.supanat.supanat_resturant;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Pc on 19/10/2559.
+ */
+public class OrderTABLE {
+    private MySQLiteOpenHelper objMySQLitOpenHelper;
+    private SQLiteDatabase writSqLiteDatabase, readSqLiteDatabase;
+
+    public static final String ORDER_TABLE = "orderTABLE";
+    public static final String COLUMN_ID_ORDER = "_id";
+    public static final String COLUMN_OFFICER ="Officer";
+    public static final String COLUMN_DESK ="Desk";
+    public static final String COLUME_FOOD="Food";
+    public static final String COLUMN_ITEM = "Item";
+
+    public OrderTABLE(Context context){
+        objMySQLitOpenHelper = new MySQLiteOpenHelper(context);
+        writSqLiteDatabase = objMySQLitOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = objMySQLitOpenHelper.getReadableDatabase();
+    }//constructor
+    public long addOrder(String strOfficer, String strDesk , String strFood , String strItem){
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(COLUMN_OFFICER,strOfficer);
+        objContentValues.put(COLUMN_DESK,strDesk);
+        objContentValues.put(COLUME_FOOD,strFood);
+        objContentValues.put(COLUMN_ITEM,strItem);
+        return writSqLiteDatabase.insert(ORDER_TABLE,null,objContentValues);
+    }//addOrder
+}
